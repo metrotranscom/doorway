605fc42098455c782e74ed88167d3a49ae23e17f
Liann Sun <lianns@google.com>
2023-04-03 18:31:27 +0000

    chore: update tests and remove emoji :'(

--- backend/core/src/seeder/seed.ts
+++ backend/core/src/seeder/seed.ts
@@ -199,7 +199,7 @@ const seedListings = async (
     listing.applicationMethods = [applicationMethods]
     // Namespace all Doorway seeded listings to better understand what is
     // local vs. not
-    listing.name = "[🚪] " + listing.name
+    listing.name = "[doorway] " + listing.name
     await listingRepository.save(listing)
 
     seeds.push(listing)

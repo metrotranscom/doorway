950e5e326f978c32c9acbec23877185b232b2118
Inés Franch <inesfranch@google.com>
2023-04-11 10:53:01 -0700

    fix: strongly type file service interface as return value of the provider

--- shared-helpers/src/utilities/pdfs.ts
+++ shared-helpers/src/utilities/pdfs.ts
@@ -1,5 +1,5 @@
 import { ListingEvent, ListingEventType } from "@bloom-housing/backend-core/types"
-import { FileServiceProvider } from "@bloom-housing/shared-services"
+import { FileServiceInterface, FileServiceProvider } from "@bloom-housing/shared-services"
 
 export const cloudinaryPdfFromId = (publicId: string, cloudName: string) => {
   return `https://res.cloudinary.com/${cloudName}/image/upload/${publicId}.pdf`
@@ -10,10 +10,10 @@ export const pdfUrlFromListingEvents = (
   listingEventType: ListingEventType
 ) => {
   const event = events.find((event) => event?.type === listingEventType)
-  const cloudinaryFileService = new FileServiceProvider().getService()
+  const fileService: FileServiceInterface = new FileServiceProvider().getService()
   if (event) {
     return event.file?.label == "cloudinaryPDF"
-      ? cloudinaryFileService.getDownloadUrlForPdf(event.file.fileId)
+      ? fileService.getDownloadUrlForPdf(event.file.fileId)
       : event.url ?? null
   }
   return null
--- shared-services/index.ts
+++ shared-services/index.ts
@@ -1,1 +1,2 @@
 export * from "./src/files/file-service.provider"
+export * from "./src/files/file-service.interface"

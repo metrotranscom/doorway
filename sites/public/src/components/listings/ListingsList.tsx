bb508e881e49e2db3d4e274908f0e6b4d89de5cd
Inés Franch <inesfranch@google.com>
2023-05-12 12:42:43 -0700

    feat: style info cards and buttons, update colors, add new text

--- sites/public/src/components/listings/ListingsList.tsx
+++ sites/public/src/components/listings/ListingsList.tsx
@@ -1,12 +1,24 @@
 import * as React from "react"
 import { getListings } from "../../lib/helpers"
 import { Listing } from "@bloom-housing/backend-core"
+import { Button, InfoCard, t } from "@bloom-housing/doorway-ui-components"
 
 type ListingsListProps = {
   listings: Listing[]
 }
 
 const ListingsList = (props: ListingsListProps) => {
-  return <div className="listingsList">{getListings(props.listings)}</div>
+  return <div>
+    <div className="listingsList">{getListings(props.listings)}</div>
+    <InfoCard title={t("t.signUpForAlerts")} subtitle={t("t.subscribeToNewsletter")} className="is-normal-primary-lighter">
+      <Button className="is-primary">{t("t.signUp")}</Button>
+    </InfoCard>
+    <InfoCard title={t("t.needHelp")} subtitle={t("t.emergencyShelter")} className="is-normal-secondary-lighter">
+    <Button className="is-secondary">{t("t.helpCenter")}</Button>
+    </InfoCard>
+    <InfoCard title={t("t.housingInSanFrancisco")} subtitle={t("t.seeSanFranciscoListings")} className="is-normal-secondary-lighter">
+    <Button className="is-secondary">{t("t.seeListings")}</Button>
+    </InfoCard>
+  </div>
 }
 export { ListingsList as default, ListingsList }

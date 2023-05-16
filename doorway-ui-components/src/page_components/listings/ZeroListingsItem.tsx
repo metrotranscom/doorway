8e29b691fabdce57032ba4ef473c06fa84e185a4
Inés Franch <inesfranch@google.com>
2023-05-16 16:31:49 -0700

    feat: change title color and add bottom border

--- doorway-ui-components/src/page_components/listings/ZeroListingsItem.tsx
+++ doorway-ui-components/src/page_components/listings/ZeroListingsItem.tsx
@@ -1,11 +1,5 @@
 import * as React from "react"
-import {
-  ResponsiveContentList,
-  ResponsiveContentItem,
-  ResponsiveContentItemHeader,
-  ResponsiveContentItemBody,
-} from "../../sections/ResponsiveContentList"
-import { Button, Heading } from "../../.."
+import { Heading } from "../../.."
 import "./ZeroListingsItem.scss"
 import Markdown from "markdown-to-jsx"
 
@@ -18,7 +12,7 @@ export interface ZeroListingsItemProps {
 
 export const ZeroListingsItem = (props: ZeroListingsItemProps) => (
   <div className="zero-listings">
-    <Heading styleType={"largePrimary"}>{props.title}</Heading>
+    <Heading styleType={"largePrimary"} className="zero-listings-title">{props.title}</Heading>
     <div className="zero-listings-description">{props.description}</div>
     {typeof props.children == "string" ? (
         <div className="markdown info-card__content">

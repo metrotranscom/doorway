67213f9d8db0c5d7bdd8206f95226f93c5c9af21
Inés Franch <inesfranch@google.com>
2023-05-22 13:58:53 -0700

    feat: change listings list and map to have vertical layout for mobile

--- sites/public/src/components/listings/ListingsCombined.tsx
+++ sites/public/src/components/listings/ListingsCombined.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { useEffect, useState } from "react"
 import { Listing } from "@bloom-housing/backend-core/types"
 import { ListingsMap } from "./ListingsMap"
 import { ListingsList } from "./ListingsList"
@@ -7,16 +7,50 @@ import styles from "./ListingsCombined.module.scss"
 type ListingsCombinedProps = {
   listings: Listing[]
   googleMapsApiKey: string
+  desktopMinWidth?: number
 }
 
-const ListingsCombined = (props: ListingsCombinedProps) => (
-  <div className={styles["listings-combined"]}>
-    <div className={styles["listings-map"]}>
-      <ListingsMap listings={props.listings} googleMapsApiKey={props.googleMapsApiKey} />
+const ListingsCombined = (props: ListingsCombinedProps) => {
+  const [isDesktop, setIsDesktop] = useState(true)
+
+  const DESKTOP_MIN_WIDTH = props.desktopMinWidth || 767 // @screen md
+  // Enables toggling off navbar links when entering mobile
+  useEffect(() => {
+    if (window.innerWidth > DESKTOP_MIN_WIDTH) {
+      setIsDesktop(true)
+    } else {
+      setIsDesktop(false)
+    }
+
+    const updateMedia = () => {
+      if (window.innerWidth > DESKTOP_MIN_WIDTH) {
+        setIsDesktop(true)
+      } else {
+        setIsDesktop(false)
+      }
+    }
+    window.addEventListener("resize", updateMedia)
+    return () => window.removeEventListener("resize", updateMedia)
+  }, [DESKTOP_MIN_WIDTH])
+
+  return isDesktop ? (
+    <div className={styles["listings-combined"]}>
+      <div className={styles["listings-map"]}>
+        <ListingsMap listings={props.listings} googleMapsApiKey={props.googleMapsApiKey} />
+      </div>
+      <div className={styles["listings-list"]}>
+        <ListingsList listings={props.listings}></ListingsList>
+      </div>
     </div>
-    <div className={styles["listings-list"]}>
-      <ListingsList listings={props.listings}></ListingsList>
+  ) : (
+    <div className={styles["listings-combined__mobile"]}>
+      <div className={styles["listings-map__mobile"]}>
+        <ListingsMap listings={props.listings} googleMapsApiKey={props.googleMapsApiKey} />
+      </div>
+      <div className={styles["listings-list__mobile"]}>
+        <ListingsList listings={props.listings}></ListingsList>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 export { ListingsCombined as default, ListingsCombined }

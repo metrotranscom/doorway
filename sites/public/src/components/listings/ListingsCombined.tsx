bbe80bdaa4915a767644e453a16a9e71c7eba9a1
Inés Franch <inesfranch@google.com>
2023-05-24 13:45:31 -0700

    feat: add swipe div for mobile listings view

--- sites/public/src/components/listings/ListingsCombined.tsx
+++ sites/public/src/components/listings/ListingsCombined.tsx
@@ -29,34 +29,58 @@ const ListingsCombined = (props: ListingsCombinedProps) => {
     preventScrollOnSwipe: true,
   })
 
+  const swipeUpHandler = useSwipeable({
+    onSwipedUp: () => {
+      setShowListingsList(false)
+      setShowListingsMap(false)
+    },
+    preventScrollOnSwipe: true,
+  })
+
+  const swipeDownHandler = useSwipeable({
+    onSwipedDown: () => {
+      setShowListingsList(false)
+      setShowListingsMap(false)
+    },
+    preventScrollOnSwipe: true,
+  })
+
   return showListingsList ? (
     <div className={styles["listings-combined"]}>
-      <div className={styles["listings-list-expanded"]}>
-        <ListingsList
-          listings={props.listings}
-          currentPage={props.currentPage}
-          lastPage={props.lastPage}
-          onPageChange={props.onPageChange}
-        ></ListingsList>
+      <div className={styles["listings-region"]}>
+        <div className={styles["swipe-area"]} {...swipeDownHandler}></div>
+        <div className={styles["listings-list-expanded"]}>
+          <ListingsList
+            listings={props.listings}
+            currentPage={props.currentPage}
+            lastPage={props.lastPage}
+            onPageChange={props.onPageChange}
+          ></ListingsList>
+        </div>
       </div>
     </div>
   ) : showListingsMap ? (
     <div className={styles["listings-combined"]}>
-      <div className={styles["listings-map"]}>
+      <div className={styles["listings-map-expanded"]}>
         <ListingsMap listings={props.listings} googleMapsApiKey={props.googleMapsApiKey} />
+      </div>
+      <div className={styles["swipe-area-fixed"]} {...swipeUpHandler}>
+        Swipe here
       </div>
     </div>
   ) : (
     <div className={styles["listings-combined"]}>
       <ListingsMap listings={props.listings} googleMapsApiKey={props.googleMapsApiKey} />
-      <div className={styles["listings-swipe-handler"]} {...swipeHandler}></div>
-      <div className={styles["listings-list"]}>
-        <ListingsList
-          listings={props.listings}
-          currentPage={props.currentPage}
-          lastPage={props.lastPage}
-          onPageChange={props.onPageChange}
-        ></ListingsList>
+      <div className={styles["listings-list-region"]}>
+        <div className={styles["swipe-area"]} {...swipeHandler}></div>
+        <div className={styles["listings-list"]}>
+          <ListingsList
+            listings={props.listings}
+            currentPage={props.currentPage}
+            lastPage={props.lastPage}
+            onPageChange={props.onPageChange}
+          ></ListingsList>
+        </div>
       </div>
     </div>
   )

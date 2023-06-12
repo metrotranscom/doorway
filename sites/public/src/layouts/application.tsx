8fe1d01d08039a8eff4f158285d7d4ca870f8fad
Inés Franch <inesfranch@google.com>
2023-06-12 16:38:57 -0700

    feat: restyle footer and add logos

--- sites/public/src/layouts/application.tsx
+++ sites/public/src/layouts/application.tsx
@@ -21,14 +21,44 @@ const Layout = (props) => {
       </div>
 
       <SiteFooter>
-        <FooterNav>
-          <Link href="/privacy">{t("pageTitle.privacy")}</Link>
-          <Link href="/disclaimer">{t("pageTitle.termsOfUse")}</Link>
-          <Link href="/disclaimer">{t("pageTitle.bahfaNonDiscriminationStatement")}</Link>
-          <Link href="/disclaimer">{t("pageTitle.languageAssistance")}</Link>
-        </FooterNav>
-        <FooterSection className="bg-gray-950" small>
-          {t("footer.bahfaCopyright")}
+        <FooterSection sectionClassName="">
+          <div className="flex-1 is-normal text-left">
+            <img
+              className="h-16 w-16"
+              src="/images/bahfa-logo.png"
+              alt="Bay Area Housing Finance Authority Logo"
+            />
+            <img
+              className="h-400 w-400"
+              src="/images/mtc-abag-logo.svg"
+              alt="Association of Bay Area Governments - Metropolitan Transporation Commission Logo"
+            />
+            <p>
+              375 Beale Street, Suite 800
+              <br /> San Francisco, CA 94105-2066
+              <br /> Monday-Friday 9:00am - 5:00pm
+              <br />
+              bahfa@bayareametro.gov
+            </p>
+          </div>
+          <div className="flex-1 is-normal text-left">
+            <FooterNav>
+              <Link href="/privacy">{t("pageTitle.privacy")}</Link>
+              <Link href="/disclaimer">{t("pageTitle.termsOfUse")}</Link>
+              <Link href="/disclaimer">{t("pageTitle.bahfaNonDiscriminationStatement")}</Link>
+              <Link href="/disclaimer">{t("pageTitle.languageAssistance")}</Link>
+            </FooterNav>
+          </div>
+        </FooterSection>
+        <FooterSection className="bg-gray-950" small sectionClassName="space-between">
+          <div className="flex-1">{t("footer.bahfaCopyright")}</div>
+          <div className="flex-1">
+            <img
+              className="h-16 w-16"
+              src="/images/eho-logo.svg"
+              alt="Equal Housing Opportunity Logo"
+            />
+          </div>
         </FooterSection>
       </SiteFooter>
     </div>

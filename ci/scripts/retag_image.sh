56f1d2b1f6432863d5e632c7939bbd95359c386a
Chris Casto <chriscasto@google.com>
2023-06-13 02:11:19 +0000

    feat: add migrate buildspecs

--- ci/scripts/retag_image.sh
+++ ci/scripts/retag_image.sh
@@ -8,8 +8,6 @@ echo "Pulling image manifest for [${REPO_NAME}:${SRC_TAG}]"
 
 MANIFEST=$(aws ecr batch-get-image --repository-name "${REPO_NAME}" --image-ids imageTag="${SRC_TAG}" --output json | jq --raw-output --join-output '.images[0].imageManifest')
 
-echo "${MANIFEST}"
-
 # The image has to have this tag or else we can't do anything
 if [[ -z "${MANIFEST}" ]]; then
     echo "No matching image found for repo [${REPO_NAME}] and tag [${SRC_TAG}]"

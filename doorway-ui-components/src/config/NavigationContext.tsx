cc77b95c8d24f5ddcbc1629feb5967ca40e83903
Liann Sun <lianns@google.com>
2023-06-22 00:04:16 +0000

    chore: remove debugger

--- doorway-ui-components/src/config/NavigationContext.tsx
+++ doorway-ui-components/src/config/NavigationContext.tsx
@@ -38,7 +38,6 @@ export interface NavigationContextProps {
 
 export const NavigationContext = createContext<NavigationContextProps>({
   LinkComponent: (props) => {
-    debugger
     const { className, linkRef, ...defaultProps } = props
     return (
       <a className={className} ref={linkRef} {...defaultProps}>

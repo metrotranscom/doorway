14564d3d7cdb1e4b2eab3b6ed851c2d96f1da257
Phatty Arbuckle <phatty@google.com>
2023-06-22 21:04:53 -0400

    remove from migration as it won't do anything

--- backend/core/src/migration/1675268723433-migration-reset.ts
+++ backend/core/src/migration/1675268723433-migration-reset.ts
@@ -383,7 +383,7 @@ export class migrationReset1675268723433 implements MigrationInterface {
     for (const jurisdictionName of [CountyCode.bay_area]) {
       await queryRunner.query(
         `INSERT INTO "jurisdictions" (name, rental_assistance_default, partner_terms) VALUES ($1, $2, $3)`,
-        [jurisdictionName, jurisDefault, "Please review terms here: https://mtc.ca.gov/doorway-housing-portal-terms-use"]
+        [jurisdictionName, jurisDefault, "Example Terms Go here:"]
       )
     }
     for (const unitType of [

bloom-housing/bloom

feat: upgrading typeorm (#3340)
* feat: upgrading typeorm

* fix: updates for tests

* fix: updating nestjs packages, clean up per morgan

* fix: undoing nestjs bump

* fix: missed changes

* fix: minor typeorm version update

* fix: user update per sean

* fix: listings update per sean

* fix: test updates

* fix: hopefully resolves failing build on heroku

* fix: attempt 2 on fixing heroku deploy

* fix: updates

* fix: update throttling in tests

* fix: asset test fix

* fix: updates for images and multiselect

* fix: update for mfa

* fix: type fix

* fix: another type fix

* fix: update for partner test

* fix: issue with jurisdiction resolver service

* fix: test fixes

* fix: fix for mfa tests?

* fix: attempt at mfa test fix

* fix: remove listing service from afs cron job (#3428)

* fix: update for tests

* fix: fix for dup resolution

---------

Co-authored-by: Morgan Ludtke <ludtkemorgan@gmail.com>
Co-authored-by: Sean Albert <smabert@gmail.com>
Co-authored-by: Morgan Ludtke <42942267+ludtkemorgan@users.noreply.github.com>

backend/core/src/seeder/seeds/ami-charts/triton-ami-chart.ts
@@ -557,7 +557,7 @@ export const itemInfo = [
 export class AmiDefaultTriton extends AmiChartDefaultSeed {
   async seed() {
     const alamedaJurisdiction = await this.jurisdictionRepository.findOneOrFail({
-      name: CountyCode.alameda,
+      where: { name: CountyCode.alameda },
     })
     return await this.amiChartRepository.save({
       name: "San Jose TCAC 2019",

backend/core/src/seeder/seeds/listings/listing-coliseum-seed.ts
@@ -151,32 +151,37 @@ export class ListingColiseumSeed extends ListingDefaultSeed {
   async seed() {
     const priorityTypeMobilityAndHearingWithVisual = await this.unitAccessibilityPriorityTypeRepository.findOneOrFail(
       {
-        name: PriorityTypes.mobilityHearingVisual,
+        where: { name: PriorityTypes.mobilityHearingVisual },
       }
     )
     const priorityTypeMobilityAndMobilityWithHearingAndVisual = await this.unitAccessibilityPriorityTypeRepository.findOneOrFail(
       {
-        name: PriorityTypes.mobilityHearingVisual,
+        where: { name: PriorityTypes.mobilityHearingVisual },
       }
     )
     const priorityTypeMobilityAndHearing = await this.unitAccessibilityPriorityTypeRepository.findOneOrFail(
       {
-        name: PriorityTypes.mobilityHearing,
+        where: { name: PriorityTypes.mobilityHearing },
       }
     )
     const priorityMobility = await this.unitAccessibilityPriorityTypeRepository.findOneOrFail({
-      name: PriorityTypes.mobility,
+      where: { name: PriorityTypes.mobility },
     })
-    const unitTypeOneBdrm = await this.unitTypeRepository.findOneOrFail({ name: "oneBdrm" })
-    const unitTypeTwoBdrm = await this.unitTypeRepository.findOneOrFail({ name: "twoBdrm" })
-    const unitTypeThreeBdrm = await this.unitTypeRepository.findOneOrFail({ name: "threeBdrm" })
+    const unitTypeOneBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "oneBdrm" },
+    })
+    const unitTypeTwoBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "twoBdrm" },
+    })
+    const unitTypeThreeBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "threeBdrm" },
+    })
 
     const alamedaJurisdiction = await this.jurisdictionRepository.findOneOrFail({
-      name: CountyCode.alameda,
+      where: { name: CountyCode.alameda },
     })
     const amiChart = await this.amiChartRepository.findOneOrFail({
-      name: "AlamedaCountyTCAC2021",
-      jurisdiction: alamedaJurisdiction,
+      where: { name: "AlamedaCountyTCAC2021", jurisdiction: { name: alamedaJurisdiction.name } },
     })
 
     const coliseumUnits: Array<UnitSeedType> = [
@@ -973,37 +978,37 @@ export class ListingColiseumSeed extends ListingDefaultSeed {
       listingMultiselectQuestions: [
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getLiveWorkPreference(alamedaJurisdiction.name).text,
+            where: { text: getLiveWorkPreference(alamedaJurisdiction.name).text },
           }),
           ordinal: 1,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getPbvPreference(alamedaJurisdiction.name).text,
+            where: { text: getPbvPreference(alamedaJurisdiction.name).text },
           }),
           ordinal: 2,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getHopwaPreference(alamedaJurisdiction.name).text,
+            where: { text: getHopwaPreference(alamedaJurisdiction.name).text },
           }),
           ordinal: 3,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getDisplaceePreference(alamedaJurisdiction.name).text,
+            where: { text: getDisplaceePreference(alamedaJurisdiction.name).text },
           }),
           ordinal: 4,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getServedInMilitaryProgram(alamedaJurisdiction.name).text,
+            where: { text: getServedInMilitaryProgram(alamedaJurisdiction.name).text },
           }),
           ordinal: 1,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getTayProgram(alamedaJurisdiction.name).text,
+            where: { text: getTayProgram(alamedaJurisdiction.name).text },
           }),
           ordinal: 2,
         },

backend/core/src/seeder/seeds/listings/listing-default-bmr-chart-seed.ts
@@ -10,15 +10,23 @@ export class ListingDefaultBmrChartSeed extends ListingDefaultSeed {
     const listing = await super.seed()
     const defaultUnits = getDefaultUnits()
 
-    const unitTypeOneBdrm = await this.unitTypeRepository.findOneOrFail({ name: "oneBdrm" })
-    const unitTypeTwoBdrm = await this.unitTypeRepository.findOneOrFail({ name: "twoBdrm" })
+    const unitTypeOneBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "oneBdrm" },
+    })
+    const unitTypeTwoBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "twoBdrm" },
+    })
 
     const alamedaJurisdiction = await this.jurisdictionRepository.findOneOrFail({
-      name: CountyCode.alameda,
+      where: { name: CountyCode.alameda },
     })
     const amiChart = await this.amiChartRepository.findOneOrFail({
-      name: defaultAmiChart.name,
-      jurisdiction: alamedaJurisdiction,
+      where: {
+        name: defaultAmiChart.name,
+        jurisdiction: {
+          name: alamedaJurisdiction.name,
+        },
+      },
     })
 
     const bmrUnits = [
@@ -32,7 +40,7 @@ export class ListingDefaultBmrChartSeed extends ListingDefaultSeed {
       listingMultiselectQuestions: [
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getTayProgram(alamedaJurisdiction.name).text,
+            where: { text: getTayProgram(alamedaJurisdiction.name).text },
           }),
           ordinal: 1,
         },

backend/core/src/seeder/seeds/listings/listing-default-seed.ts
@@ -54,19 +54,25 @@ export class ListingDefaultSeed {
 
   async seed() {
     const priorityTypeMobilityAndHearing = await this.unitAccessibilityPriorityTypeRepository.findOneOrFail(
-      { name: PriorityTypes.mobilityHearing }
+      { where: { name: PriorityTypes.mobilityHearing } }
     )
-    const unitTypeOneBdrm = await this.unitTypeRepository.findOneOrFail({ name: "oneBdrm" })
-    const unitTypeTwoBdrm = await this.unitTypeRepository.findOneOrFail({ name: "twoBdrm" })
+    const unitTypeOneBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "oneBdrm" },
+    })
+    const unitTypeTwoBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "twoBdrm" },
+    })
     const alamedaJurisdiction = await this.jurisdictionRepository.findOneOrFail({
-      name: CountyCode.alameda,
+      where: { name: CountyCode.alameda },
     })
     const amiChart = await this.amiChartRepository.findOneOrFail({
-      name: "AlamedaCountyTCAC2021",
-      jurisdiction: alamedaJurisdiction,
+      where: {
+        name: "AlamedaCountyTCAC2021",
+        jurisdiction: {
+          name: alamedaJurisdiction.name,
+        },
+      },
     })
-
-    const defaultImage = await this.assetsRepository.save(getDefaultAssets()[0])
 
     const listingCreateDto: Omit<
       DeepPartial<Listing>,
@@ -80,54 +86,48 @@ export class ListingDefaultSeed {
       listingMultiselectQuestions: [
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getLiveWorkPreference(alamedaJurisdiction.name).text,
+            where: { text: getLiveWorkPreference(alamedaJurisdiction.name).text },
           }),
           ordinal: 1,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getDisplaceePreference(alamedaJurisdiction.name).text,
+            where: { text: getDisplaceePreference(alamedaJurisdiction.name).text },
           }),
           ordinal: 2,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getServedInMilitaryProgram(alamedaJurisdiction.name).text,
+            where: { text: getServedInMilitaryProgram(alamedaJurisdiction.name).text },
           }),
           ordinal: 1,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getTayProgram(alamedaJurisdiction.name).text,
+            where: { text: getTayProgram(alamedaJurisdiction.name).text },
           }),
           ordinal: 2,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getDisabilityOrMentalIllnessProgram(alamedaJurisdiction.name).text,
+            where: { text: getDisabilityOrMentalIllnessProgram(alamedaJurisdiction.name).text },
           }),
           ordinal: 3,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getHousingSituationProgram(alamedaJurisdiction.name).text,
+            where: { text: getHousingSituationProgram(alamedaJurisdiction.name).text },
           }),
           ordinal: 4,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getFlatRentAndRentBasedOnIncomeProgram(alamedaJurisdiction.name).text,
+            where: { text: getFlatRentAndRentBasedOnIncomeProgram(alamedaJurisdiction.name).text },
           }),
           ordinal: 5,
         },
       ],
       events: getDefaultListingEvents(),
-      images: [
-        {
-          image: defaultImage,
-          ordinal: 1,
-        },
-      ],
       jurisdictionName: "Alameda",
       jurisdiction: alamedaJurisdiction,
     }

metrotranscom/doorway

backend/core/src/seeder/seeds/ami-charts/triton-ami-chart.ts
@@ -557,7 +557,7 @@ export const itemInfo = [
 export class AmiDefaultTriton extends AmiChartDefaultSeed {
   async seed() {
     const bayAreaJurisdiction = await this.jurisdictionRepository.findOneOrFail({
-      name: CountyCode.bay_area,
+      where: { name: CountyCode.bay_area },
     })
     return await this.amiChartRepository.save({
       name: "San Jose TCAC 2019",

backend/core/src/seeder/seeds/listings/listing-coliseum-seed.ts
@@ -151,32 +151,38 @@ export class ListingColiseumSeed extends ListingDefaultSeed {
   async seed() {
     const priorityTypeMobilityAndHearingWithVisual = await this.unitAccessibilityPriorityTypeRepository.findOneOrFail(
       {
-        name: PriorityTypes.mobilityHearingVisual,
+        where: { name: PriorityTypes.mobilityHearingVisual },
       }
     )
     const priorityTypeMobilityAndMobilityWithHearingAndVisual = await this.unitAccessibilityPriorityTypeRepository.findOneOrFail(
       {
-        name: PriorityTypes.mobilityHearingVisual,
+        where: { name: PriorityTypes.mobilityHearingVisual },
       }
     )
     const priorityTypeMobilityAndHearing = await this.unitAccessibilityPriorityTypeRepository.findOneOrFail(
       {
-        name: PriorityTypes.mobilityHearing,
+        where: { name: PriorityTypes.mobilityHearing },
       }
     )
     const priorityMobility = await this.unitAccessibilityPriorityTypeRepository.findOneOrFail({
-      name: PriorityTypes.mobility,
+      where: { name: PriorityTypes.mobility },
     })
-    const unitTypeOneBdrm = await this.unitTypeRepository.findOneOrFail({ name: "oneBdrm" })
-    const unitTypeTwoBdrm = await this.unitTypeRepository.findOneOrFail({ name: "twoBdrm" })
-    const unitTypeThreeBdrm = await this.unitTypeRepository.findOneOrFail({ name: "threeBdrm" })
+    const unitTypeOneBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "oneBdrm" },
+    })
+    const unitTypeTwoBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "twoBdrm" },
+    })
+    const unitTypeThreeBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "threeBdrm" },
+    })
 
     const bayAreaJurisdiction = await this.jurisdictionRepository.findOneOrFail({
-      name: CountyCode.bay_area,
+      where: { name: CountyCode.bay_area },
     })
     const amiChart = await this.amiChartRepository.findOneOrFail({
       name: "AlamedaCountyTCAC2021",
-      jurisdiction: bayAreaJurisdiction,
+      jurisdiction: { name: bayAreaJurisdiction.name },
     })
 
     const coliseumUnits: Array<UnitSeedType> = [
@@ -973,37 +979,37 @@ export class ListingColiseumSeed extends ListingDefaultSeed {
       listingMultiselectQuestions: [
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getLiveWorkPreference(bayAreaJurisdiction.name).text,
+            where: { text: getLiveWorkPreference(bayAreaJurisdiction.name).text },
           }),
           ordinal: 1,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getPbvPreference(bayAreaJurisdiction.name).text,
+            where: { text: getPbvPreference(bayAreaJurisdiction.name).text },
           }),
           ordinal: 2,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getHopwaPreference(bayAreaJurisdiction.name).text,
+            where: { text: getHopwaPreference(bayAreaJurisdiction.name).text },
           }),
           ordinal: 3,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getDisplaceePreference(bayAreaJurisdiction.name).text,
+            where: { text: getDisplaceePreference(bayAreaJurisdiction.name).text },
           }),
           ordinal: 4,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getServedInMilitaryProgram(bayAreaJurisdiction.name).text,
+            where: { text: getServedInMilitaryProgram(bayAreaJurisdiction.name).text },
           }),
           ordinal: 1,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getTayProgram(bayAreaJurisdiction.name).text,
+            where: { text: getTayProgram(bayAreaJurisdiction.name).text },
           }),
           ordinal: 2,
         },

backend/core/src/seeder/seeds/listings/listing-default-bmr-chart-seed.ts
@@ -10,15 +10,21 @@ export class ListingDefaultBmrChartSeed extends ListingDefaultSeed {
     const listing = await super.seed()
     const defaultUnits = getDefaultUnits()
 
-    const unitTypeOneBdrm = await this.unitTypeRepository.findOneOrFail({ name: "oneBdrm" })
-    const unitTypeTwoBdrm = await this.unitTypeRepository.findOneOrFail({ name: "twoBdrm" })
+    const unitTypeOneBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "oneBdrm" },
+    })
+    const unitTypeTwoBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "twoBdrm" },
+    })
 
     const bayAreaJurisdiction = await this.jurisdictionRepository.findOneOrFail({
-      name: CountyCode.bay_area,
+      where: { name: CountyCode.bay_area },
     })
     const amiChart = await this.amiChartRepository.findOneOrFail({
       name: defaultAmiChart.name,
-      jurisdiction: bayAreaJurisdiction,
+      jurisdiction: {
+        name: bayAreaJurisdiction.name,
+      },
     })
 
     const bmrUnits = [
@@ -32,7 +38,7 @@ export class ListingDefaultBmrChartSeed extends ListingDefaultSeed {
       listingMultiselectQuestions: [
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getTayProgram(bayAreaJurisdiction.name).text,
+            where: { text: getTayProgram(bayAreaJurisdiction.name).text },
           }),
           ordinal: 1,
         },

backend/core/src/seeder/seeds/listings/listing-default-seed.ts
@@ -54,20 +54,26 @@ export class ListingDefaultSeed {
 
   async seed() {
     const priorityTypeMobilityAndHearing = await this.unitAccessibilityPriorityTypeRepository.findOneOrFail(
-      { name: PriorityTypes.mobilityHearing }
+      { where: { name: PriorityTypes.mobilityHearing } }
     )
-    const unitTypeOneBdrm = await this.unitTypeRepository.findOneOrFail({ name: "oneBdrm" })
-    const unitTypeTwoBdrm = await this.unitTypeRepository.findOneOrFail({ name: "twoBdrm" })
+    const unitTypeOneBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "oneBdrm" },
+    })
+    const unitTypeTwoBdrm = await this.unitTypeRepository.findOneOrFail({
+      where: { name: "twoBdrm" },
+    })
     const bayAreaJurisdiction = await this.jurisdictionRepository.findOneOrFail({
-      name: CountyCode.bay_area,
+      where: { name: CountyCode.bay_area },
     })
     // todo update the name of this
     const amiChart = await this.amiChartRepository.findOneOrFail({
-      name: "AlamedaCountyTCAC2021",
-      jurisdiction: bayAreaJurisdiction,
+      where: {
+        name: "AlamedaCountyTCAC2021",
+        jurisdiction: {
+          name: bayAreaJurisdiction.name,
+        },
+      },
     })
-
-    const defaultImage = await this.assetsRepository.save(getDefaultAssets()[0])
 
     const listingCreateDto: Omit<
       DeepPartial<Listing>,
@@ -81,54 +87,48 @@ export class ListingDefaultSeed {
       listingMultiselectQuestions: [
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getLiveWorkPreference(bayAreaJurisdiction.name).text,
+            where: { text: getLiveWorkPreference(bayAreaJurisdiction.name).text },
           }),
           ordinal: 1,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getDisplaceePreference(bayAreaJurisdiction.name).text,
+            where: { text: getDisplaceePreference(bayAreaJurisdiction.name).text },
           }),
           ordinal: 2,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getServedInMilitaryProgram(bayAreaJurisdiction.name).text,
+            where: { text: getServedInMilitaryProgram(bayAreaJurisdiction.name).text },
           }),
           ordinal: 1,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getTayProgram(bayAreaJurisdiction.name).text,
+            where: { text: getTayProgram(bayAreaJurisdiction.name).text },
           }),
           ordinal: 2,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getDisabilityOrMentalIllnessProgram(bayAreaJurisdiction.name).text,
+            where: { text: getDisabilityOrMentalIllnessProgram(bayAreaJurisdiction.name).text },
           }),
           ordinal: 3,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getHousingSituationProgram(bayAreaJurisdiction.name).text,
+            where: { text: getHousingSituationProgram(bayAreaJurisdiction.name).text },
           }),
           ordinal: 4,
         },
         {
           multiselectQuestion: await this.multiselectQuestionsRepository.findOneOrFail({
-            text: getFlatRentAndRentBasedOnIncomeProgram(bayAreaJurisdiction.name).text,
+            where: { text: getFlatRentAndRentBasedOnIncomeProgram(bayAreaJurisdiction.name).text },
           }),
           ordinal: 5,
         },
       ],
       events: getDefaultListingEvents(),
-      images: [
-        {
-          image: defaultImage,
-          ordinal: 1,
-        },
-      ],
       jurisdictionName: "Bay Area",
       jurisdiction: bayAreaJurisdiction,
     }
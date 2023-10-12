a5650fbb3e419efef4ffe398a4995f1310aa8384
ColinBuyck <colin.buyck@gmail.com>
2023-10-12 00:00:47 -0700

    fix: ami + listing migration

--- backend/core/src/migration/1695143897902-add-jurisdictions.ts
+++ backend/core/src/migration/1695143897902-add-jurisdictions.ts
@@ -11,6 +11,7 @@ export class addJurisdictions1695143897902 implements MigrationInterface {
         WHERE name='Bay Area'`
       )
     )[0]
+    //add new jurisdictions
     const countyArr = ["Contra Costa", "Marin", "Napa", "Santa Clara", "Solano", "Sonoma"]
     countyArr.forEach(async (county) => {
       const newJuris = { ...baseJurisdiction, name: county }
@@ -26,26 +27,60 @@ export class addJurisdictions1695143897902 implements MigrationInterface {
       )
     })
 
-    // add new jurisdiction info
+    //link existing listings to corresponding jurisdiction
     const existingListings = await queryRunner.query(
-      `SELECT id,county
+      `SELECT listings.id,county
        FROM listings 
        LEFT JOIN address on listings.building_address_id = address.id`
     )
-    const existingJurisdictionIds = await queryRunner.query(
+    const existingJurisdictions = await queryRunner.query(
       `SELECT id,name
        FROM jurisdictions`
     )
 
     existingListings.forEach(async (listing) => {
-      const matchingJuris = existingJurisdictionIds.find((juris) => juris.name === listing.county)
-        .id
+      const matchingJuris = existingJurisdictions.find((juris) => juris.name === listing.county).id
       await queryRunner.query(
         `UPDATE listings
-        SET jurisdiction_id=${matchingJuris}
-        WHERE id=${listing.id}`
+        SET jurisdiction_id='${matchingJuris}'
+        WHERE id='${listing.id}'`
       )
     })
+
+    //link existing ami charts to new jurisdictions
+    const amiJurisMap = {
+      "Marin - HUD": "Marin",
+      "Napa - HUD": "Napa",
+      "Contra Costa - HUD": "Contra Costa",
+      "Santa Clara - HUD": "Santa Clara",
+      "Solano - HUD": "Solano",
+      "Sonoma - HUD": "Sonoma",
+      "Sonoma - Sonoma County State and Local": "Sonoma",
+      "Contra Costa - CA TCAC": "Contra Costa",
+      "Marin - CA TCAC": "Marin",
+      "Napa - CA TCAC": "Napa",
+      "Santa Clara - CA TCAC": "Santa Clara",
+      "Solano - CA TCAC": "Solano",
+      "Sonoma - CA TCAC": "Sonoma",
+      "Oakland 2023": "Bay Area",
+    }
+    const amiCharts = await queryRunner.query(
+      `SELECT id,name
+       FROM ami_chart`
+    )
+    amiCharts.forEach(async (ami) => {
+      const matchingJuris = existingJurisdictions.find(
+        (juris) => juris.name === amiJurisMap[ami.name]
+      )?.id
+      if (matchingJuris) {
+        await queryRunner.query(
+          `UPDATE ami_chart
+          SET jurisdiction_id='${matchingJuris}'
+          WHERE id='${ami.id}'`
+        )
+      }
+    })
   }
+
   public async down(queryRunner: QueryRunner): Promise<void> {}
 }

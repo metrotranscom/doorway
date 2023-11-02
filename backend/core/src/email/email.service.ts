baaf0ee4f5d93d2e2fbccb53464727806608ff66
ColinBuyck <colin.buyck@gmail.com>
2023-11-02 14:21:57 -0700

    fix: wip css loading issue

--- backend/core/src/email/email.service.ts
+++ backend/core/src/email/email.service.ts
@@ -21,7 +21,7 @@ import { Translation } from "../translations/entities/translation.entity"
 import { IdName } from "../../types"
 import { formatLocalDate } from "../shared/utils/format-local-date"
 import axios from "axios"
-import { xmlEmailBuilder } from "./helpers"
+import { govEmailBuilder, templateDeconstructor } from "./helpers"
 
 type EmailAttachmentData = {
   data: string
@@ -201,22 +201,23 @@ export class EmailService {
   }
 
   public async forgotPassword(user: User, appUrl: string) {
-    await this.govSend(
-      this.template("listing-opportunity")({
-        appUrl,
-        tableRows: [
-          { label: "Community", value: "Senior 55+" },
-          { label: "Applications Due", value: "August 11, 2021" },
-          { label: "Address", value: "2330 Webster Street, Oakland CA 94612" },
-          { label: "1 Bedrooms", value: "1 unit, 1 bath, 668 sqft" },
-          { label: "2 Bedrooms", value: "2 units, 1-2 baths, 900 - 968 sqft" },
-          { label: "Rent", value: "$1,251 - $1,609 per month" },
-          { label: "Minimum Income", value: "$2,502 - $3,218 per month" },
-          { label: "Maximum Income", value: "$6,092 - $10,096 per month" },
-          { label: "Lottery Date", value: "August 31, 2021" },
-        ],
-      })
-    )
+    const compiledEmail = this.template("listing-opportunity")({
+      appUrl,
+      tableRows: [
+        { label: "Community", value: "Senior 55+" },
+        { label: "Applications Due", value: "August 11, 2021" },
+        { label: "Address", value: "2330 Webster Street, Oakland CA 94612" },
+        { label: "1 Bedrooms", value: "1 unit, 1 bath, 668 sqft" },
+        { label: "2 Bedrooms", value: "2 units, 1-2 baths, 900 - 968 sqft" },
+        { label: "Rent", value: "$1,251 - $1,609 per month" },
+        { label: "Minimum Income", value: "$2,502 - $3,218 per month" },
+        { label: "Maximum Income", value: "$6,092 - $10,096 per month" },
+        { label: "Lottery Date", value: "August 31, 2021" },
+      ],
+    })
+    // const deconstructedObj = templateDeconstructor(compiledEmail)
+    await this.govSend(compiledEmail)
+
     // const jurisdiction = await this.getUserJurisdiction(user)
     // void (await this.loadTranslations(jurisdiction, user.language))
     // const compiledTemplate = this.template("forgot-password")
@@ -252,22 +253,22 @@ export class EmailService {
     }
 
     // TODO: This is mock data just for the template that will need to be updated
-    await this.govSend(
-      compiledTemplate({
-        appUrl,
-        tableRows: [
-          { label: "Community", value: "Senior 55+" },
-          { label: "Applications Due", value: "August 11, 2021" },
-          { label: "Address", value: "2330 Webster Street, Oakland CA 94612" },
-          { label: "1 Bedrooms", value: "1 unit, 1 bath, 668 sqft" },
-          { label: "2 Bedrooms", value: "2 units, 1-2 baths, 900 - 968 sqft" },
-          { label: "Rent", value: "$1,251 - $1,609 per month" },
-          { label: "Minimum Income", value: "$2,502 - $3,218 per month" },
-          { label: "Maximum Income", value: "$6,092 - $10,096 per month" },
-          { label: "Lottery Date", value: "August 31, 2021" },
-        ],
-      })
-    )
+    // await this.govSend(
+    //   compiledTemplate({
+    //     appUrl,
+    //     tableRows: [
+    //       { label: "Community", value: "Senior 55+" },
+    //       { label: "Applications Due", value: "August 11, 2021" },
+    //       { label: "Address", value: "2330 Webster Street, Oakland CA 94612" },
+    //       { label: "1 Bedrooms", value: "1 unit, 1 bath, 668 sqft" },
+    //       { label: "2 Bedrooms", value: "2 units, 1-2 baths, 900 - 968 sqft" },
+    //       { label: "Rent", value: "$1,251 - $1,609 per month" },
+    //       { label: "Minimum Income", value: "$2,502 - $3,218 per month" },
+    //       { label: "Maximum Income", value: "$6,092 - $10,096 per month" },
+    //       { label: "Lottery Date", value: "August 31, 2021" },
+    //     ],
+    //   })
+    // )
   }
 
   private async loadTranslations(jurisdiction: Jurisdiction | null, language: Language) {
@@ -344,18 +345,20 @@ export class EmailService {
     return partials
   }
 
-  private async govSend(emailBody: string) {
-    console.log(xmlEmailBuilder)
+  // private async govSend(emailObj?: { body: string; head: string }) {
+  private async govSend(emailObj?: string) {
+    // console.log(xmlEmailBuilder())
     console.log(process.env.GOVDELIVERY_ACCOUNT_CODE)
     console.log(process.env.GOVDELIVERY_USERNAME, process.env.GOVDELIVERY_PASSWORD)
     await axios.post(
-      `http://stage-api.govdelivery.com/api/account/${process.env.GOVDELIVERY_ACCOUNT_CODE}/bulletins/send_now`,
-      xmlEmailBuilder(emailBody),
+      `https://stage-api.govdelivery.com/api/account/${process.env.GOVDELIVERY_ACCOUNT_CODE}/bulletins/send_now`,
+      govEmailBuilder(emailObj),
       {
-        headers: { "Content-Type": "text/xml", charset: "utf-8" },
-        auth: {
-          username: process.env.GOVDELIVERY_USERNAME,
-          password: process.env.GOVDELIVERY_PASSWORD,
+        headers: {
+          "Content-Type": "application/xml",
+          Authorization: `Basic ${Buffer.from(
+            `${process.env.GOVDELIVERY_USERNAME}:${process.env.GOVDELIVERY_PASSWORD}`
+          ).toString("base64")}`,
         },
       }
     )
--- backend/core/src/email/helpers.ts
+++ backend/core/src/email/helpers.ts
@@ -1,21 +1,15 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
-export const xmlEmailBuilder = (emailBody: string) =>
-  "<bulletin>" +
-  `<header>Test Header</header>` +
-  `<subject>Test Subject</subject>` +
-  "<body>" +
-  "<![CDATA[" +
-  `${emailBody}` +
-  "]]></body>" +
-  `<sms_body nil='true'></sms_body>` +
-  `<publish_rss type='boolean'>false</publish_rss>` +
-  `<open_tracking type='boolean'>true</open_tracking>` +
-  `<click_tracking type='boolean'>true</click_tracking>` +
-  `<share_content_enabled type='boolean'>true</share_content_enabled>` +
-  `<topics type='array'>` +
-  `<topic>` +
-  `<code>doorway-listings</code>` +
-  `</topic>` +
-  `</topics>` +
-  `<categories type='array' />` +
-  `</bulletin>`
+export const govEmailBuilder = (emailBody?: string, emailHead?: string) => {
+  console.log("boink", emailHead)
+  return `<bulletin>\n   <header>Test Header</header>\n   <subject>Test email send</subject>\n  <body><![CDATA[\n     ${emailBody}\n   ]]></body>\n   <footer>ENTER FOOTER</footer>\n   <sms_body nil='true'></sms_body>\n   <publish_rss type='boolean'>false</publish_rss>\n   <open_tracking type='boolean'>true</open_tracking>\n   <click_tracking type='boolean'>true</click_tracking>\n   <share_content_enabled type='boolean'>true</share_content_enabled>\n   <topics type='array'>\n     <topic>\n       <code>doorway-listings</code>\n     </topic>\n   </topics>\n   <categories type='array' />\n </bulletin>`
+}
+
+export const templateDeconstructor = (compiledEmail: string) => {
+  console.log(compiledEmail)
+  const headRegEx = /<head>([\s\S]*?)<\/head>/
+  const headText = headRegEx.exec(compiledEmail)?.[1]
+  const bodyRegEx = /<body.*([\s\S]*?)<\/body>/
+  const bodyText = bodyRegEx.exec(compiledEmail)?.[1]
+  console.log({ head: headText, body: bodyText })
+  return { head: headText, body: bodyText }
+}

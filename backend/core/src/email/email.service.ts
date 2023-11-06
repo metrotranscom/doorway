b359487e57bd018b942a78c957010f9f78e3cf91
ColinBuyck <colin.buyck@gmail.com>
2023-11-06 14:55:21 -0800

    fix: updated env approach

--- backend/core/src/email/email.service.ts
+++ backend/core/src/email/email.service.ts
@@ -328,17 +328,48 @@ export class EmailService {
     return partials
   }
 
+  private isGovConfigured(): boolean {
+    const {
+      GOVDELIVERY_API_URL,
+      GOVDELIVERY_USERNAME,
+      GOVDELIVERY_PASSWORD,
+      GOVDELIVERY_TOPIC,
+    } = process.env
+    return (
+      !!GOVDELIVERY_API_URL &&
+      !!GOVDELIVERY_USERNAME &&
+      !!GOVDELIVERY_PASSWORD &&
+      !!GOVDELIVERY_TOPIC
+    )
+  }
+
   private async govSend(rawHtml: string, subject: string) {
+    const {
+      GOVDELIVERY_API_URL,
+      GOVDELIVERY_USERNAME,
+      GOVDELIVERY_PASSWORD,
+      GOVDELIVERY_TOPIC,
+    } = process.env
+    const isGovConfigured =
+      !!GOVDELIVERY_API_URL &&
+      !!GOVDELIVERY_USERNAME &&
+      !!GOVDELIVERY_PASSWORD &&
+      !!GOVDELIVERY_TOPIC
+    if (!isGovConfigured) {
+      console.warn("failed to configure Govdelivery, ensure that all env variables are provided")
+      return
+    }
+
     // juice inlines css to allow for email styling
     const inlineHtml = juice(rawHtml)
     const govEmailXml = `<bulletin>\n <subject>${subject}</subject>\n  <body><![CDATA[\n     
-      ${inlineHtml}\n   ]]></body>\n   <sms_body nil='true'></sms_body>\n   <publish_rss type='boolean'>false</publish_rss>\n   <open_tracking type='boolean'>true</open_tracking>\n   <click_tracking type='boolean'>true</click_tracking>\n   <share_content_enabled type='boolean'>true</share_content_enabled>\n   <topics type='array'>\n     <topic>\n       <code>doorway-listings</code>\n     </topic>\n   </topics>\n   <categories type='array' />\n </bulletin>`
+      ${inlineHtml}\n   ]]></body>\n   <sms_body nil='true'></sms_body>\n   <publish_rss type='boolean'>false</publish_rss>\n   <open_tracking type='boolean'>true</open_tracking>\n   <click_tracking type='boolean'>true</click_tracking>\n   <share_content_enabled type='boolean'>true</share_content_enabled>\n   <topics type='array'>\n     <topic>\n       <code>${GOVDELIVERY_TOPIC}</code>\n     </topic>\n   </topics>\n   <categories type='array' />\n </bulletin>`
 
-    await axios.post(process.env.GOVDELIVERY_API_URL, govEmailXml, {
+    await axios.post(GOVDELIVERY_API_URL, govEmailXml, {
       headers: {
         "Content-Type": "application/xml",
         Authorization: `Basic ${Buffer.from(
-          `${process.env.GOVDELIVERY_USERNAME}:${process.env.GOVDELIVERY_PASSWORD}`
+          `${GOVDELIVERY_USERNAME}:${GOVDELIVERY_PASSWORD}`
         ).toString("base64")}`,
       },
     })

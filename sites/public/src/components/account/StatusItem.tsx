2fe7277bebb1bc22dd0ee6e0eab066ecf337c74e
ColinBuyck <colin.buyck@gmail.com>
2024-02-14 17:00:34 -0800

    fix: more accessible styling

--- sites/public/src/components/account/StatusItem.tsx
+++ sites/public/src/components/account/StatusItem.tsx
@@ -30,7 +30,7 @@ const StatusItem = (props: StatusItemProps) => {
           <h3 className={styles["status-item__title"]}>{props.listingName}</h3>
           <p className={styles["status-item__status"]}>
             {props.strings?.status ?? t("application.status")}:{" "}
-            <Tag variant="highlight-cool">
+            <Tag variant="primary-inverse">
               {props.strings?.submittedStatus ?? t("application.statuses.submitted")}
             </Tag>
           </p>

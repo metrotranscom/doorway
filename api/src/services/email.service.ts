eb788b2d7e94c2eac680b91335895627c4bce972
emilyjablonski <emijablonski@gmail.com>
2024-05-03 17:31:16 -0400

    refactor: pr feedback

--- api/src/services/email.service.ts
+++ api/src/services/email.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpService } from '@nestjs/axios';
 import fs from 'fs';
@@ -155,7 +159,7 @@ export class EmailService {
     } catch (e) {
       console.log(e);
       console.error('Failed to send email');
-      return e;
+      throw new InternalServerErrorException(e);
     }
   }
 

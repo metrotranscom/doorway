95675ba3fd0e1ebf2094a107b1c812f125a8f2ac
Glenn Brown <gbrown@bayareametro.gov>
2025-02-03 15:39:28 -0800

    feat: make it configurable

--- api/prisma/seed-staging.ts
+++ api/prisma/seed-staging.ts
@@ -1337,7 +1337,18 @@ export const stagingSeed = async (
       }
     },
   );
-  for (let i = 0; i < 100; i++) {
+  await prismaClient.userAccounts.create({
+    data: await userFactory({
+      roles: { isAdmin: true },
+      email: `admin@example.com`,
+      confirmedAt: new Date(),
+      jurisdictionIds: [jurisdiction.id],
+      acceptedTerms: true,
+      password: 'abcdef',
+    }),
+  });
+  const adminAccounts: number = process.env.ADMIN_ACCOUNTS != undefined ? Number(process.env.ADMIN_ACCOUNTS) : 1
+  for (let i = 0; i < adminAccounts; i++) {
     await prismaClient.userAccounts.create({
       data: await userFactory({
         roles: { isAdmin: true },

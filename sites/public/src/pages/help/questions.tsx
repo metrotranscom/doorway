17cc79a91a42dc74042657e341dc1dc564fe8bd8
ColinBuyck <colin.buyck@gmail.com>
2025-03-04 19:00:52 -0800

    fix: uptake to md pages

--- sites/public/src/pages/help/questions.tsx
+++ sites/public/src/pages/help/questions.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useContext } from "react"
-import { PageHeader, t } from "@bloom-housing/ui-components"
+import { t } from "@bloom-housing/ui-components"
 import { PageView, pushGtmEvent, AuthContext } from "@bloom-housing/shared-helpers"
 import { UserStatus } from "../../lib/constants"
 import Layout from "../../layouts/application"
 import { questionsLinkableCards } from "../../tsx_content/questions-cards"
 import { MetaTags } from "../../components/shared/MetaTags"
 import { DoorwayLinkableCardGroup } from "../../components/shared/DoorwayLinkableCardGroup"
+import { PageHeaderLayout } from "../../patterns/PageHeaderLayout"
 
 const FrequentlyAskedQuestions = () => {
   const { profile } = useContext(AuthContext)
@@ -22,14 +23,13 @@ const FrequentlyAskedQuestions = () => {
 
   return (
     <Layout>
-      <PageHeader title={t("pageTitle.questions")} />
-      <MetaTags title={t("pageTitle.questions")} description={metaDescription} />
-      <div className="lg:my-14">
+      <PageHeaderLayout heading={t("pageTitle.questions")}>
+        <MetaTags title={t("pageTitle.questions")} description={metaDescription} />
         <DoorwayLinkableCardGroup
           cards={questionsLinkableCards()}
           className="m-auto"
         ></DoorwayLinkableCardGroup>
-      </div>
+      </PageHeaderLayout>
     </Layout>
   )
 }
--- sites/public/src/pages/professional-partners/jurisdictions.tsx
+++ sites/public/src/pages/professional-partners/jurisdictions.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useContext } from "react"
-import { t, PageHeader } from "@bloom-housing/ui-components"
+import { t } from "@bloom-housing/ui-components"
 import { PageView, pushGtmEvent, AuthContext } from "@bloom-housing/shared-helpers"
 import { UserStatus } from "../../lib/constants"
 import Layout from "../../layouts/application"
 import { MetaTags } from "../../components/shared/MetaTags"
 import { professionalsPartnersJurisdictionsCards } from "../../tsx_content/professional-partners-jurisdictions-cards"
 import { DoorwayLinkableCardGroup } from "../../components/shared/DoorwayLinkableCardGroup"
+import { PageHeaderLayout } from "../../patterns/PageHeaderLayout"
 
 const Jurisdictions = () => {
   const { profile } = useContext(AuthContext)
@@ -22,14 +23,13 @@ const Jurisdictions = () => {
 
   return (
     <Layout>
-      <PageHeader title={t("pageTitle.jurisdictions")} />
-      <MetaTags title={t("pageTitle.jurisdictions")} description={metaDescription} />
-      <div className="lg:my-14">
+      <PageHeaderLayout heading={t("pageTitle.jurisdictions")}>
+        <MetaTags title={t("pageTitle.jurisdictions")} description={metaDescription} />
         <DoorwayLinkableCardGroup
           cards={professionalsPartnersJurisdictionsCards()}
           className="m-auto"
         ></DoorwayLinkableCardGroup>
-      </div>
+      </PageHeaderLayout>
     </Layout>
   )
 }

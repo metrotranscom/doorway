bloom-housing/bloom

feat: put My applications link behind feature flag (#4842)
* feat: put My applications link behind feature flag

* fix: test due to capitalization change

* fix: more capitalization updates for tests

* fix: additional partners test fix

* test: add unit test for dashboard without My applications

* fix: lint

* fix: add redirect effect hooks for pages of feature flags

* fix: linting

* chore: revert previous language changes

* chore: additional revert

sites/public/src/layouts/application.tsx
@@ -133,7 +133,7 @@ const getHeaderLinks = (
   profile: User,
   signOut: () => Promise<void>,
   addToast: (message: string, props: ToastProps) => void,
-  showFavorites: boolean
+  linksBehindFlags: Record<string, boolean>
 ) => {
   const headerLinks: HeaderLink[] = [
     {
@@ -155,11 +155,15 @@ const getHeaderLinks = (
           label: t("nav.myDashboard"),
           href: "/account/dashboard",
         },
-        {
-          label: t("account.myApplications"),
-          href: "/account/applications",
-        },
-        ...(showFavorites
+        ...(linksBehindFlags["applications"]
+          ? [
+              {
+                label: t("account.myApplications"),
+                href: "/account/applications",
+              },
+            ]
+          : []),
+        ...(linksBehindFlags["favorites"]
           ? [
               {
                 label: t("account.myFavorites"),
@@ -199,12 +203,12 @@ const Layout = (props) => {
   const router = useRouter()
 
   const [showFavorites, setShowFavorites] = useState(false)
+  const [showApplications, setShowApplications] = useState(true)
 
   useEffect(() => {
-    if (window.localStorage.getItem("bloom-show-favorites-menu-item") === "true") {
-      setShowFavorites(true)
-    }
-  }, [setShowFavorites])
+    setShowFavorites(window.localStorage.getItem("bloom-show-favorites-menu-item") === "true")
+    setShowApplications(window.localStorage.getItem("bloom-hide-applications-menu-item") !== "true")
+  }, [setShowFavorites, setShowApplications])
 
   const languages =
     router?.locales?.map((item) => ({
@@ -229,7 +233,10 @@ const Layout = (props) => {
                 active: t("config.routePrefix") === lang.prefix,
               }
             })}
-            links={getHeaderLinks(router, profile, signOut, addToast, showFavorites)}
+            links={getHeaderLinks(router, profile, signOut, addToast, {
+              applications: showApplications,
+              favorites: showFavorites,
+            })}
             titleLink={"/"}
             logo={
               <Icon size={"lg"}>

sites/public/src/pages/account/dashboard.tsx
@@ -18,7 +18,7 @@ import Layout from "../../layouts/application"
 import { MetaTags } from "../../components/shared/MetaTags"
 import { UserStatus } from "../../lib/constants"
 import MaxWidthLayout from "../../layouts/max-width"
-import { isFeatureFlagOn } from "../../lib/helpers"
+import { isFeatureFlagOn, setFeatureFlagLocalStorage } from "../../lib/helpers"
 import { fetchJurisdictionByName } from "../../lib/hooks"
 
 import styles from "./account.module.scss"
@@ -45,11 +45,15 @@ function Dashboard(props: DashboardProps) {
       setAlertMessage(alert)
     }
 
-    window.localStorage.setItem(
-      "bloom-show-favorites-menu-item",
-      (
-        isFeatureFlagOn(props.jurisdiction, FeatureFlagEnum.enableListingFavoriting) === true
-      ).toString()
+    setFeatureFlagLocalStorage(
+      props.jurisdiction,
+      FeatureFlagEnum.enableListingFavoriting,
+      "show-favorites-menu-item"
+    )
+    setFeatureFlagLocalStorage(
+      props.jurisdiction,
+      FeatureFlagEnum.disableCommonApplication,
+      "hide-applications-menu-item"
     )
   }, [router, props.jurisdiction, profile])
 
@@ -75,26 +79,30 @@ function Dashboard(props: DashboardProps) {
             <h1 className={"sr-only"}>{t("nav.myDashboard")}</h1>
             <Grid spacing="lg" className={styles["account-card-container"]}>
               <Grid.Row columns={2}>
-                <Grid.Cell>
-                  <BloomCard
-                    iconSymbol="application"
-                    title={t("account.myApplications")}
-                    subtitle={t("account.myApplicationsSubtitle")}
-                    variant={"block"}
-                    headingPriority={2}
-                  >
-                    <Card.Section>
-                      <Button
-                        size="sm"
-                        href={"/account/applications"}
-                        variant="primary-outlined"
-                        id="account-dashboard-applications"
-                      >
-                        {t("account.viewApplications")}
-                      </Button>
-                    </Card.Section>
-                  </BloomCard>
-                </Grid.Cell>
+                {isFeatureFlagOn(props.jurisdiction, FeatureFlagEnum.disableCommonApplication) ? (
+                  <></>
+                ) : (
+                  <Grid.Cell>
+                    <BloomCard
+                      iconSymbol="application"
+                      title={t("account.myApplications")}
+                      subtitle={t("account.myApplicationsSubtitle")}
+                      variant={"block"}
+                      headingPriority={2}
+                    >
+                      <Card.Section>
+                        <Button
+                          size="sm"
+                          href={"/account/applications"}
+                          variant="primary-outlined"
+                          id="account-dashboard-applications"
+                        >
+                          {t("account.viewApplications")}
+                        </Button>
+                      </Card.Section>
+                    </BloomCard>
+                  </Grid.Cell>
+                )}
                 <Grid.Cell>
                   <BloomCard
                     iconSymbol="profile"

metrotranscom/doorway

sites/public/src/layouts/application.tsx
@@ -19,12 +19,12 @@ const Layout = (props: LayoutProps) => {
   const router = useRouter()
 
   const [showFavorites, setShowFavorites] = useState(false)
+  const [showApplications, setShowApplications] = useState(true)
 
   useEffect(() => {
-    if (window.localStorage.getItem("bloom-show-favorites-menu-item") === "true") {
-      setShowFavorites(true)
-    }
-  }, [setShowFavorites])
+    setShowFavorites(window.localStorage.getItem("bloom-show-favorites-menu-item") === "true")
+    setShowApplications(window.localStorage.getItem("bloom-hide-applications-menu-item") !== "true")
+  }, [setShowFavorites, setShowApplications])
 
   const languages =
     router?.locales?.map((item) => ({
@@ -91,10 +91,14 @@ const Layout = (props: LayoutProps) => {
             title: t("nav.myDashboard"),
             href: "/account/dashboard",
           },
-          {
-            title: t("account.myApplications"),
-            href: "/account/applications",
-          },
+          ...(showApplications
+            ? [
+                {
+                  title: t("account.myApplications"),
+                  href: "/account/applications",
+                },
+              ]
+            : []),
           ...(showFavorites
             ? [
                 {

sites/public/src/pages/account/dashboard.tsx
@@ -18,7 +18,7 @@ import Layout from "../../layouts/application"
 import { MetaTags } from "../../components/shared/MetaTags"
 import { UserStatus } from "../../lib/constants"
 import MaxWidthLayout from "../../layouts/max-width"
-import { isFeatureFlagOn } from "../../lib/helpers"
+import { isFeatureFlagOn, setFeatureFlagLocalStorage } from "../../lib/helpers"
 import { fetchJurisdictionByName } from "../../lib/hooks"
 
 import styles from "./account.module.scss"
@@ -45,11 +45,15 @@ function Dashboard(props: DashboardProps) {
       setAlertMessage(alert)
     }
 
-    window.localStorage.setItem(
-      "bloom-show-favorites-menu-item",
-      (
-        isFeatureFlagOn(props.jurisdiction, FeatureFlagEnum.enableListingFavoriting) === true
-      ).toString()
+    setFeatureFlagLocalStorage(
+      props.jurisdiction,
+      FeatureFlagEnum.enableListingFavoriting,
+      "show-favorites-menu-item"
+    )
+    setFeatureFlagLocalStorage(
+      props.jurisdiction,
+      FeatureFlagEnum.disableCommonApplication,
+      "hide-applications-menu-item"
     )
   }, [router, props.jurisdiction, profile])
 
@@ -75,26 +79,30 @@ function Dashboard(props: DashboardProps) {
             <h1 className={"sr-only"}>{t("nav.myDashboard")}</h1>
             <Grid spacing="lg" className={styles["account-card-container"]}>
               <Grid.Row columns={2}>
-                <Grid.Cell>
-                  <BloomCard
-                    customIcon="application"
-                    title={t("account.myApplications")}
-                    subtitle={t("account.myApplicationsSubtitle")}
-                    variant={"block"}
-                    headingPriority={2}
-                  >
-                    <Card.Section>
-                      <Button
-                        size="sm"
-                        href={"/account/applications"}
-                        variant="primary-outlined"
-                        id="account-dashboard-applications"
-                      >
-                        {t("account.viewApplications")}
-                      </Button>
-                    </Card.Section>
-                  </BloomCard>
-                </Grid.Cell>
+                {isFeatureFlagOn(props.jurisdiction, FeatureFlagEnum.disableCommonApplication) ? (
+                  <></>
+                ) : (
+                  <Grid.Cell>
+                    <BloomCard
+                      customIcon="application"
+                      title={t("account.myApplications")}
+                      subtitle={t("account.myApplicationsSubtitle")}
+                      variant={"block"}
+                      headingPriority={2}
+                    >
+                      <Card.Section>
+                        <Button
+                          size="sm"
+                          href={"/account/applications"}
+                          variant="primary-outlined"
+                          id="account-dashboard-applications"
+                        >
+                          {t("account.viewApplications")}
+                        </Button>
+                      </Card.Section>
+                    </BloomCard>
+                  </Grid.Cell>
+                )}
                 <Grid.Cell>
                   <BloomCard
                     customIcon="profile"
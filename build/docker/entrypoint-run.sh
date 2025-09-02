fb3e9a2348f58b406120a8a80212c83bd09d4697
Glenn Brown <gbrown@bayareametro.gov>
2025-09-02 09:11:59 -0700

    fix: debugging docker

--- build/docker/entrypoint-run.sh
+++ build/docker/entrypoint-run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 export DATABASE_URL="postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}"
+echo "DATABASE_URL: $DATABASE_URL"
+echo "Running migrations..."
 yarn prisma generate&&yarn start:prod

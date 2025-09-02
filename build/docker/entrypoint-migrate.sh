fd5ac0324b07d89a7c614e281b49cd9506ec583f
Glenn Brown <gbrown@bayareametro.gov>
2025-09-02 13:13:18 -0700

    fix: more debugging

--- build/docker/entrypoint-migrate.sh
+++ build/docker/entrypoint-migrate.sh
@@ -25,7 +25,7 @@ ENCODED_PASSWORD=$(node -e "console.log(encodeURIComponent('$PGPASSWORD'))")
 export DATABASE_URL="postgresql://${PGUSER}:${ENCODED_PASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}"
 echo "DATABASE_URL (password masked): postgresql://${PGUSER}:***@${PGHOST}:${PGPORT}/${PGDATABASE}"
 
-echo "Running migrations..."
-if [ "$SKIP_MIGRATIONS" = TRUE ]; then yarn db:migration:skip 00_init || true && yarn db:migration:skip 02_hba_to_prisma || true && yarn db:migration:skip 03_0_external_listing || true && yarn db:migration:run; \
-    else yarn ${MIGRATION_CMD}; \
+
+if [ "$SKIP_MIGRATIONS" = TRUE ]; then echo "Skipping Migrations"&&yarn db:migration:skip 00_init || true && yarn db:migration:skip 02_hba_to_prisma || true && yarn db:migration:skip 03_0_external_listing || true && yarn db:migration:run; \
+    else echo "Running migrations..."&&yarn ${MIGRATION_CMD}; \
     fi

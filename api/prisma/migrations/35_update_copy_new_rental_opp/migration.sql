6a4786e0a2ca285a25d59d9e069f5c69a6ed77f5
juan <juancarlosmillord@gmail.com>
2025-11-04 15:38:25 -0400

    updated translation function values

--- api/prisma/migrations/35_update_copy_new_rental_opp/migration.sql
+++ api/prisma/migrations/35_update_copy_new_rental_opp/migration.sql
@@ -3,8 +3,8 @@ UPDATE
 SET
   translations = jsonb_set(
     translations,
-    '{rentalOpportunity}',
-    '{"viewListingNotice": "THIS INFORMATION MAY CHANGE - Please view listing for the most updated information"}'
+    '{rentalOpportunity, viewListingNotice}',
+    '"THIS INFORMATION MAY CHANGE - Please view listing for the most updated information"'
   )
 WHERE
   language = 'en';
@@ -15,8 +15,8 @@ UPDATE
 SET
   translations = jsonb_set(
     translations,
-    '{rentalOpportunity}',
-   '{"viewListingNotice": "ESTA INFORMACIÓN PUEDE CAMBIAR: consulte la lista para obtener la información más actualizada"}'
+    '{rentalOpportunity, viewListingNotice}',
+   '"ESTA INFORMACIÓN PUEDE CAMBIAR: consulte la lista para obtener la información más actualizada"'
   )
 WHERE
   language = 'es';
@@ -26,8 +26,8 @@ UPDATE
 SET
   translations = jsonb_set(
     translations,
-    '{rentalOpportunity}',
-   '{"viewListingNotice": "此資訊可能會更改 - 請查看列表以獲取最新信息"}'
+    '{rentalOpportunity, viewListingNotice}',
+   '"此資訊可能會更改 - 請查看列表以獲取最新信息"'
   )
 WHERE
   language = 'zh';
@@ -37,8 +37,8 @@ UPDATE
 SET
   translations = jsonb_set(
     translations,
-    '{rentalOpportunity}',
-    '{"viewListingNotice": "THÔNG TIN NÀY CÓ THỂ THAY ĐỔI - Vui lòng xem danh sách để biết thông tin cập nhật nhất"}'
+    '{rentalOpportunity,viewListingNotice}',
+    '"THÔNG TIN NÀY CÓ THỂ THAY ĐỔI - Vui lòng xem danh sách để biết thông tin cập nhật nhất"'
   )
 WHERE
   language = 'vi';
@@ -50,8 +50,8 @@ UPDATE
 SET
   translations = jsonb_set(
     translations,
-    '{rentalOpportunity}',
-    '{"viewListingNotice": "MAAARING MAGBAGO ANG IMPORMASYON NA ITO - Mangyaring tingnan ang listahan para sa pinakabagong impormasyon"}'
+    '{rentalOpportunity, viewListingNotice}',
+    '"MAAARING MAGBAGO ANG IMPORMASYON NA ITO - Mangyaring tingnan ang listahan para sa pinakabagong impormasyon"'
   )
 WHERE
   language = 'tl';

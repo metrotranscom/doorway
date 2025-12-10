97fa64c4c2962941fc3f44c4314717ff737074c6
Morgan Ludtke <42942267+ludtkemorgan@users.noreply.github.com>
2025-12-09 18:57:29 -0600

    fix: show lottery tab for waitlist lottery (#5673)

--- sites/partners/src/pages/listings/[id]/lottery.tsx
+++ sites/partners/src/pages/listings/[id]/lottery.tsx
@@ -363,7 +363,8 @@ const Lottery = (props: { listing: Listing | undefined }) => {
                 lotteryLabel:
                   listing.status === ListingsStatusEnum.closed &&
                   listing?.lotteryOptIn &&
-                  listing?.reviewOrderType === ReviewOrderTypeEnum.lottery
+                  (listing?.reviewOrderType === ReviewOrderTypeEnum.lottery ||
+                    listing?.reviewOrderType === ReviewOrderTypeEnum.waitlistLottery)
                     ? t("listings.lotteryTitle")
                     : undefined,
               }}

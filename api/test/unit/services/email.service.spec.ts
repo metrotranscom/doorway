f0c042ba9bcc6d36358193f4de1aa6e2046df5b0
juan <juancarlosmillord@gmail.com>
2025-12-10 11:06:04 -0400

    updating test copy

--- api/test/unit/services/email.service.spec.ts
+++ api/test/unit/services/email.service.spec.ts
@@ -838,8 +838,8 @@ describe('Testing email service', () => {
       expect(emailMock).toMatch(
         '<span class="intro">Rental opportunity at</span> <br />test listing',
       );
-      expect(emailMock).toMatch(
-        'THIS INFORMATION MAY CHANGE <br /> - Please view listing for the most updated information',
+      expect(emailMock).toContain(
+        'THIS INFORMATION MAY CHANGE &lt;br /&gt; - Please view listing for the most updated information',
       );
       expect(emailMock).toMatch(
         /<td class="bold">\s*Applications Due\s*<\/td>/,

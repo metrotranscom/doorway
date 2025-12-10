35ce0871501f5c6174d689aca92fef676cd49db1
juan <juancarlosmillord@gmail.com>
2025-12-10 12:26:26 -0400

    updated migration

--- api/prisma/migrations/41_doorway_account_req_field/migration.sql
+++ api/prisma/migrations/41_doorway_account_req_field/migration.sql
@@ -3,8 +3,14 @@ UPDATE
 SET
   translations = jsonb_set(
     translations,
-    '{rentalOpportunity, doorwayAccountRequirement}',
-    '"You will need a Doorway account in order to apply. Receipt of this email does not equate to a Doorway account"'
+    '{rentalOpportunity}',
+    '{
+        "doorwayAccountRequirement": "you will need a Doorway account in order to apply. Receipt of this email does not equate to a Doorway account",
+        "resetRequest":
+          "We received a request to reset your password for your Doorway Housing Portal account. You must click the following link to complete the reset:",
+        "viewListingNotice":
+          "THIS INFORMATION MAY CHANGE <br /> - Please view listing for the most updated information"
+        }'
   )
 WHERE
   language = 'en';
@@ -13,10 +19,16 @@ WHERE
 UPDATE
   translations
 SET
-  translations = jsonb_set(
+   translations = jsonb_set(
     translations,
-    '{rentalOpportunity, doorwayAccountRequirement}',
-   '"Necesitará una cuenta Doorway para poder presentar la solicitud. La recepción de este correo electrónico no equivale a una cuenta Doorway"'
+    '{rentalOpportunity}',
+    '{
+        "doorwayAccountRequirement": "Necesitará una cuenta Doorway para poder presentar la solicitud. La recepción de este correo electrónico no equivale a una cuenta Doorway",
+        "resetRequest":
+          "We received a request to reset your password for your Doorway Housing Portal account. You must click the following link to complete the reset:",
+        "viewListingNotice":
+          "ESTA INFORMACIÓN PUEDE CAMBIAR <br />  consulte la lista para obtener la información más actualizada"
+        }'
   )
 WHERE
   language = 'es';
@@ -24,10 +36,16 @@ WHERE
 UPDATE
   translations
 SET
-  translations = jsonb_set(
+    translations = jsonb_set(
     translations,
-    '{rentalOpportunity, doorwayAccountRequirement}',
-   '"您需要一個 Doorway 帳戶才能申請。收到此電子郵件並不等於擁有 Doorway 帳戶"'
+    '{rentalOpportunity}',
+    '{
+        "doorwayAccountRequirement": "您需要一個 Doorway 帳戶才能申請。收到此電子郵件並不等於擁有 Doorway 帳戶",
+        "resetRequest":
+          "We received a request to reset your password for your Doorway Housing Portal account. You must click the following link to complete the reset:",
+        "viewListingNotice":
+          "此資訊可能會更改 <br /> - 請查看列表以獲取最新信息"
+        }'
   )
 WHERE
   language = 'zh';
@@ -35,10 +53,16 @@ WHERE
 UPDATE
   translations
 SET
-  translations = jsonb_set(
+    translations = jsonb_set(
     translations,
-    '{rentalOpportunity,doorwayAccountRequirement}',
-    '"Bạn sẽ cần có tài khoản Doorway để đăng ký. Việc nhận được email này không tương đương với tài khoản Doorway"'
+    '{rentalOpportunity}',
+    '{
+        "doorwayAccountRequirement": "Bạn sẽ cần có tài khoản Doorway để đăng ký. Việc nhận được email này không tương đương với tài khoản Doorway",
+        "resetRequest":
+          "We received a request to reset your password for your Doorway Housing Portal account. You must click the following link to complete the reset:",
+        "viewListingNotice":
+          "THÔNG TIN NÀY CÓ THỂ THAY ĐỔI <br /> - Vui lòng xem danh sách để biết thông tin cập nhật nhất"
+        }'
   )
 WHERE
   language = 'vi';
@@ -48,10 +72,16 @@ WHERE
 UPDATE
   translations
 SET
-  translations = jsonb_set(
+   translations = jsonb_set(
     translations,
-    '{rentalOpportunity, doorwayAccountRequirement}',
-    '"Kakailanganin mo ng Doorway account para makapag-apply. Ang pagtanggap ng email na ito ay hindi katumbas ng isang Doorway account"'
+    '{rentalOpportunity}',
+    '{
+        "doorwayAccountRequirement": "Kakailanganin mo ng Doorway account para makapag-apply. Ang pagtanggap ng email na ito ay hindi katumbas ng isang Doorway account",
+        "resetRequest":
+          "We received a request to reset your password for your Doorway Housing Portal account. You must click the following link to complete the reset:",
+        "viewListingNotice":
+          "MAAARING MAGBAGO ANG IMPORMASYON NA ITO <br /> - Mangyaring tingnan ang listahan para sa pinakabagong impormasyon"
+        }'
   )
 WHERE
   language = 'tl';

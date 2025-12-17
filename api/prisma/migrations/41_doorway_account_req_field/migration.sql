cc36c0b4627e1cc52ba3485cb898c6b66f584139
juan <juancarlosmillord@gmail.com>
2025-12-17 15:50:13 -0400

    updating migration file correctly with two lines

--- api/prisma/migrations/41_doorway_account_req_field/migration.sql
+++ api/prisma/migrations/41_doorway_account_req_field/migration.sql
@@ -15,14 +15,21 @@ UPDATE
 SET
   translations = jsonb_set(
     translations,
-    '{rentalOpportunity, viewListingNotice}',
-   '{
-    "line1: "THIS INFORMATION MAY CHANGE",
-    "line2: "- Please view listing for the most updated information"
-   }'
+    '{rentalOpportunity, viewListingNotice, line1}',
+   '"THIS INFORMATION MAY CHANGE"'
   )
 WHERE
   language = 'en';
 
 
 
+UPDATE
+  translations
+SET
+  translations = jsonb_set(
+    translations,
+    '{rentalOpportunity, viewListingNotice, line2}',
+    '"- Please view listing for the most updated information"'
+  )
+WHERE
+  language = 'en';

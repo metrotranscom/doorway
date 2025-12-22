c362f76d32341cc8a1eece79e7d779d3bd07bde5
juan <juancarlosmillord@gmail.com>
2025-12-22 12:32:53 -0400

    added missing lang

--- api/prisma/migrations/41_add_back_change_pass_text/migration.sql
+++ api/prisma/migrations/41_add_back_change_pass_text/migration.sql
@@ -8,3 +8,52 @@ SET
   )
 WHERE
   language = 'en';
+
+
+UPDATE
+  translations
+SET
+  translations = jsonb_set(
+    translations,
+    '{forgotPassword, changePassword}',
+   '"Cambiar mi contraseña"'
+  )
+WHERE
+  language = 'es';
+
+
+  UPDATE
+  translations
+SET
+  translations = jsonb_set(
+    translations,
+    '{forgotPassword, changePassword}',
+   '"更改我的密碼"'
+  )
+WHERE
+  language = 'zh';
+
+
+  UPDATE
+  translations
+SET
+  translations = jsonb_set(
+    translations,
+    '{forgotPassword, changePassword}',
+   '"Thay đổi mật khẩu của tôi"'
+  )
+WHERE
+  language = 'vi';
+
+
+
+  UPDATE
+  translations
+SET
+  translations = jsonb_set(
+    translations,
+    '{forgotPassword, changePassword}',
+   '"Baguhin ang aking password"'
+  )
+WHERE
+  language = 'tl';

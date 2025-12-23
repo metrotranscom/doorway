bloom-housing/bloom

fix: warn of user deletion cron job (#5652)
* fix: warn of user deletion cron job

* fix: add cron string to template

* fix: typo fix

* fix: test rebase issues

* fix: reorder user controller endpoints

api/prisma/seed-staging.ts
@@ -332,14 +332,12 @@ export const stagingSeed = async (
   });
   // add jurisdiction specific translations and default ones
   await prismaClient.translations.create({
-    data: translationFactory(mainJurisdiction.id, mainJurisdiction.name),
+    data: translationFactory({
+      jurisdiction: { id: mainJurisdiction.id, name: mainJurisdiction.name },
+    }),
   });
   await prismaClient.translations.create({
-    data: translationFactory(
-      mainJurisdiction.id,
-      mainJurisdiction.name,
-      LanguagesEnum.es,
-    ),
+    data: translationFactory({ language: LanguagesEnum.es }),
   });
   await prismaClient.translations.create({
     data: translationFactory(),

metrotranscom/doorway

api/prisma/seed-staging.ts
@@ -451,14 +451,12 @@ export const stagingSeed = async (
   });
   // add jurisdiction specific translations and default ones
   await prismaClient.translations.create({
-    data: translationFactory(jurisdiction.id, jurisdiction.name),
+    data: translationFactory({
+      jurisdiction: { id: jurisdiction.id, name: jurisdiction.name },
+    }),
   });
   await prismaClient.translations.create({
-    data: translationFactory(
-      mainJurisdiction.id,
-      mainJurisdiction.name,
-      LanguagesEnum.es,
-    ),
+    data: translationFactory({ language: LanguagesEnum.es }),
   });
   await prismaClient.translations.create({
     data: translationFactory(),
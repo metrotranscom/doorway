66fbeef3385bb2367ebdb8bd58ac0c6fa38981fb
Krzysztof Zięcina <kziecina@airnauts.com>
2025-12-23 15:51:20 +0100

    fix: add application status dropdown with tag (#5703)

--- sites/partners/src/pages/listings/[id]/applications/index.tsx
+++ sites/partners/src/pages/listings/[id]/applications/index.tsx
@@ -58,6 +58,10 @@ const ApplicationsList = () => {
     FeatureFlagEnum.disableWorkInRegion,
     listingDto?.jurisdictions.id
   )
+  const enableApplicationStatus = doJurisdictionsHaveFeatureFlagOn(
+    FeatureFlagEnum.enableApplicationStatus,
+    listingDto?.jurisdictions.id
+  )
   const includeDemographicsPartner =
     profile?.userRoles?.isPartner && listingJurisdiction?.enablePartnerDemographics
   const { onExport, exportLoading } = useZipExport(
@@ -126,8 +130,18 @@ const ApplicationsList = () => {
   }, [applications])
 
   const columnDefs = useMemo(() => {
-    return getColDefs(maxHouseholdSize, enableFullTimeStudentQuestion, disableWorkInRegion)
-  }, [maxHouseholdSize, enableFullTimeStudentQuestion, disableWorkInRegion])
+    return getColDefs(
+      maxHouseholdSize,
+      enableFullTimeStudentQuestion,
+      disableWorkInRegion,
+      enableApplicationStatus
+    )
+  }, [
+    maxHouseholdSize,
+    enableFullTimeStudentQuestion,
+    disableWorkInRegion,
+    enableApplicationStatus,
+  ])
 
   const gridComponents = {
     formatLinkCell,

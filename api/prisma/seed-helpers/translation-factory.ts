bloom-housing/bloom

fix: warn of user deletion cron job (#5652)
* fix: warn of user deletion cron job

* fix: add cron string to template

* fix: typo fix

* fix: test rebase issues

* fix: reorder user controller endpoints

api/prisma/seed-helpers/translation-factory.ts
@@ -1,7 +1,23 @@
 import { LanguagesEnum, Prisma } from '@prisma/client';
 
-const translations = (jurisdictionName?: string, language?: LanguagesEnum) => {
+const translations = (
+  jurisdiction?: {
+    id: string;
+    name: string;
+  },
+  language?: LanguagesEnum,
+) => {
   if (!language || language === LanguagesEnum.en) {
+    if (jurisdiction) {
+      return {
+        footer: {
+          line1: jurisdiction.name,
+          line2: '',
+          thankYou: 'Thank you',
+          footer: jurisdiction.name,
+        },
+      };
+    }
     return {
       t: {
         hello: 'Hello',
@@ -12,10 +28,10 @@ const translations = (jurisdictionName?: string, language?: LanguagesEnum) => {
         reviewListing: 'Review Listing',
       },
       footer: {
-        line1: `${jurisdictionName || 'Bloom'}`,
+        line1: 'Bloom',
         line2: '',
         thankYou: 'Thank you',
-        footer: `${jurisdictionName || 'Bloom Housing'}`,
+        footer: 'Bloom Housing',
       },
       header: {
         logoUrl:
@@ -193,12 +209,21 @@ const translations = (jurisdictionName?: string, language?: LanguagesEnum) => {
           'If you want to learn about how lotteries work, please see the lottery section of the',
         otherOpportunities4: 'Housing Portal Help Center',
       },
+      accountRemoval: {
+        subject: 'Bloom Housing Scheduled Account Removal Due to Inactivity',
+        courtesyText:
+          'This is a courtesy email to let you know that because your Bloom Housing Portal account has been inactive for 3 years, your account will be deleted in 30 days per our Terms of Use and Privacy Policy. If you’d like to keep your account, please log in sometime in the next month and we’ll consider your account active again.',
+        signIn: 'Sign in to Bloom Housing',
+      },
     };
   } else if (language === LanguagesEnum.es) {
     return {
-      t: { seeListing: 'VER EL LISTADO' },
+      t: {
+        hello: 'Hola',
+        seeListing: 'VER EL LISTADO',
+      },
       footer: {
-        line1: `${jurisdictionName || 'Bloom'}`,
+        line1: 'Bloom',
         line2: '',
       },
       confirmation: {
@@ -241,22 +266,42 @@ const translations = (jurisdictionName?: string, language?: LanguagesEnum) => {
           'Si desea obtener información sobre cómo funcionan las loterías, consulte la sección de lotería del',
         otherOpportunities4: 'Housing Portal Centro de ayuda',
       },
+      accountRemoval: {
+        subject:
+          'Eliminación programada de cuenta de Bloom Housing debido a inactividad',
+        courtesyText:
+          'Este es un correo electrónico de cortesía para informarle que, debido a que su cuenta del Portal de Bloom Housing ha estado inactiva durante 3 años, se eliminará en 30 días según nuestros Términos de Uso y Política de Privacidad. Si desea conservar su cuenta, inicie sesión durante el próximo mes y la consideraremos activa de nuevo.',
+        signIn: 'Iniciar sesión en Bloom Housing',
+      },
+      register: {
+        welcome: 'Bienvenido',
+        welcomeMessage:
+          'Gracias por crear su cuenta en %{appUrl}. Ahora le resultará más fácil iniciar, guardar y enviar solicitudes en línea para los anuncios que aparecen en el sitio.',
+        confirmMyAccount: 'Confirmar mi cuenta',
+        toConfirmAccountMessage:
+          'Para completar la creación de su cuenta, haga clic en el siguiente enlace:',
+      },
     };
   }
 };
 
-export const translationFactory = (
-  jurisdictionId?: string,
-  jurisdictionName?: string,
-  language?: LanguagesEnum,
-): Prisma.TranslationsCreateInput => {
+export const translationFactory = (optionalParams?: {
+  jurisdiction?: {
+    id: string;
+    name: string;
+  };
+  language?: LanguagesEnum;
+}): Prisma.TranslationsCreateInput => {
   return {
-    language: language || LanguagesEnum.en,
-    translations: translations(jurisdictionName, language),
-    jurisdictions: jurisdictionId
+    language: optionalParams?.language || LanguagesEnum.en,
+    translations: translations(
+      optionalParams?.jurisdiction,
+      optionalParams?.language,
+    ),
+    jurisdictions: optionalParams?.jurisdiction
       ? {
           connect: {
-            id: jurisdictionId,
+            id: optionalParams.jurisdiction.id,
           },
         }
       : undefined,

metrotranscom/doorway

api/prisma/seed-helpers/translation-factory.ts
@@ -1,7 +1,23 @@
 import { LanguagesEnum, Prisma } from '@prisma/client';
 
-const translations = (jurisdictionName?: string, language?: LanguagesEnum) => {
+const translations = (
+  jurisdiction?: {
+    id: string;
+    name: string;
+  },
+  language?: LanguagesEnum,
+) => {
   if (!language || language === LanguagesEnum.en) {
+    if (jurisdiction) {
+      return {
+        footer: {
+          line1: jurisdiction.name,
+          line2: '',
+          thankYou: 'Thank you',
+          footer: jurisdiction.name,
+        },
+      };
+    }
     return {
       t: {
         hello: 'Hello',
@@ -12,10 +28,10 @@ const translations = (jurisdictionName?: string, language?: LanguagesEnum) => {
         reviewListing: 'Review Listing',
       },
       footer: {
-        line1: `${jurisdictionName || 'Bloom'}`,
+        line1: 'Bloom',
         line2: '',
         thankYou: 'Thank you',
-        footer: `${jurisdictionName || 'Bloom Housing'}`,
+        footer: 'Bloom Housing',
       },
       header: {
         logoUrl: 'https://housingbayarea.mtc.ca.gov/images/doorway-logo.png',
@@ -221,12 +237,21 @@ const translations = (jurisdictionName?: string, language?: LanguagesEnum) => {
         otherOpportunities4: 'Doorway Housing Portal Help Center',
         termsOfUse: 'Terms of Use',
       },
+      accountRemoval: {
+        subject: 'Doorway Scheduled Account Removal Due to Inactivity',
+        courtesyText:
+          'This is a courtesy email to let you know that because your Doorway Housing Portal account has been inactive for 3 years, your account will be deleted in 30 days per our Terms of Use and Privacy Policy. If you’d like to keep your account, please log in sometime in the next month and we’ll consider your account active again.',
+        signIn: 'Sign in to Bloom Housing',
+      },
     };
   } else if (language === LanguagesEnum.es) {
     return {
-      t: { seeListing: 'VER EL LISTADO' },
+      t: {
+        hello: 'Hola',
+        seeListing: 'VER EL LISTADO',
+      },
       footer: {
-        line1: `${jurisdictionName || 'Bloom'}`,
+        line1: 'Bloom',
         line2: '',
       },
       confirmation: {
@@ -272,22 +297,42 @@ const translations = (jurisdictionName?: string, language?: LanguagesEnum) => {
         otherOpportunities4: 'Doorway Housing Portal Centro de ayuda',
         termsOfUse: 'Términos de uso',
       },
+      accountRemoval: {
+        subject:
+          'Eliminación programada de cuenta de Doorway debido a inactividad',
+        courtesyText:
+          'Este es un correo electrónico de cortesía para informarle que, debido a que su cuenta del Portal de Bloom Housing ha estado inactiva durante 3 años, se eliminará en 30 días según nuestros Términos de Uso y Política de Privacidad. Si desea conservar su cuenta, inicie sesión durante el próximo mes y la consideraremos activa de nuevo.',
+        signIn: 'Iniciar sesión en Bloom Housing',
+      },
+      register: {
+        welcome: 'Bienvenido',
+        welcomeMessage:
+          'Gracias por crear su cuenta en %{appUrl}. Ahora le resultará más fácil iniciar, guardar y enviar solicitudes en línea para los anuncios que aparecen en el sitio.',
+        confirmMyAccount: 'Confirmar mi cuenta',
+        toConfirmAccountMessage:
+          'Para completar la creación de su cuenta, haga clic en el siguiente enlace:',
+      },
     };
   }
 };
 
-export const translationFactory = (
-  jurisdictionId?: string,
-  jurisdictionName?: string,
-  language?: LanguagesEnum,
-): Prisma.TranslationsCreateInput => {
+export const translationFactory = (optionalParams?: {
+  jurisdiction?: {
+    id: string;
+    name: string;
+  };
+  language?: LanguagesEnum;
+}): Prisma.TranslationsCreateInput => {
   return {
-    language: language || LanguagesEnum.en,
-    translations: translations(jurisdictionName, language),
-    jurisdictions: jurisdictionId
+    language: optionalParams?.language || LanguagesEnum.en,
+    translations: translations(
+      optionalParams?.jurisdiction,
+      optionalParams?.language,
+    ),
+    jurisdictions: optionalParams?.jurisdiction
       ? {
           connect: {
-            id: jurisdictionId,
+            id: optionalParams.jurisdiction.id,
           },
         }
       : undefined,
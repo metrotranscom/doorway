14511b9a6fa25f47c2e1aebb241faaf29d9edf2d
Morgan Ludtke <ludtkemorgan@gmail.com>
2026-01-26 13:38:44 -0600

    fix: set draft applications to submitted

--- api/prisma/migrations/42_application_status_waitlist_fields/migration.sql
+++ api/prisma/migrations/42_application_status_waitlist_fields/migration.sql
@@ -1,10 +1,6 @@
-/*
-  Warnings:
-
-  - The values [draft,removed] on the enum `application_status_enum` will be removed. If these variants are still used in the database, this will fail.
-*/
 -- AlterEnum
 BEGIN;
+UPDATE applications SET status = 'submitted' where status = 'draft';
 CREATE TYPE "application_status_enum_new" AS ENUM ('submitted', 'declined', 'receivedUnit', 'waitlist', 'waitlistDeclined');
 ALTER TABLE "applications" ALTER COLUMN "status" TYPE "application_status_enum_new" USING ("status"::text::"application_status_enum_new");
 ALTER TYPE "application_status_enum" RENAME TO "application_status_enum_old";

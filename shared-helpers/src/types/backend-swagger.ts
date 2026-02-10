0f69f0dec95d1777b824a87e32d60fae982c3359
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2026-02-09 17:48:44 -0700

    feat: faq page (#5816)

--- shared-helpers/src/types/backend-swagger.ts
+++ shared-helpers/src/types/backend-swagger.ts
@@ -10017,6 +10017,7 @@ export enum FeatureFlagEnum {
   "enableProperties" = "enableProperties",
   "enableReferralQuestionUnits" = "enableReferralQuestionUnits",
   "enableRegions" = "enableRegions",
+  "enableResources" = "enableResources",
   "enableSection8Question" = "enableSection8Question",
   "enableSingleUseCode" = "enableSingleUseCode",
   "enableSmokingPolicyRadio" = "enableSmokingPolicyRadio",

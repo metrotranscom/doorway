70f2b9395a2f83049efa84d3624a07924c173c17
juanmillord <juancarlosmillord@gmail.com>
2026-02-26 11:31:49 -0400

    feat: 5472/lahd-housing-programs (#5902)
    added two new programs

--- api/prisma/seed-staging.ts
+++ api/prisma/seed-staging.ts
@@ -1007,6 +1007,81 @@ export const stagingSeed = async (
       },
     }),
   });
+  const mobilityAccessibilityNeedsProgramQuestion =
+    await prismaClient.multiselectQuestions.create({
+      data: multiselectQuestionFactory(angelopolisJurisdiction.id, {
+        multiselectQuestion: {
+          text: 'Mobility accessibility needs',
+          description:
+            'Some units require at least one resident to have a mobility accessibility need',
+          applicationSection:
+            MultiselectQuestionsApplicationSectionEnum.programs,
+          optOutText: 'None of the above',
+          options: [
+            { text: 'Wheelchair', ordinal: 0 },
+            { text: 'Walker', ordinal: 1 },
+            { text: 'Power chair', ordinal: 2 },
+            { text: 'Other mobility device', ordinal: 3 },
+          ],
+        },
+      }),
+    });
+  const hearingVisionAccessibilityNeedsProgramQuestion =
+    await prismaClient.multiselectQuestions.create({
+      data: multiselectQuestionFactory(angelopolisJurisdiction.id, {
+        multiselectQuestion: {
+          text: 'Hearing/vision accessibility needs',
+          description:
+            'Some units require at least one resident to have a hearing / vision accessibility need',
+          applicationSection:
+            MultiselectQuestionsApplicationSectionEnum.programs,
+          optOutText: 'None of the above',
+          options: [
+            { text: 'Audible and visual doorbells', ordinal: 0 },
+            {
+              text: 'Fire and smoke alarms with hard wired strobes',
+              ordinal: 1,
+            },
+            {
+              text: 'Documents in screen-reader accessible format',
+              ordinal: 2,
+            },
+            { text: 'Documents in large text or braille', ordinal: 3 },
+          ],
+        },
+      }),
+    });
+
+  const multiselectQuestionPrograms =
+    await prismaClient.multiselectQuestions.create({
+      data: multiselectQuestionFactory(mainJurisdiction.id, {
+        multiselectQuestion: {
+          text: 'Housing Situation',
+          description:
+            'Thinking about the past 30 days, do either of these describe your housing situation?',
+          applicationSection:
+            MultiselectQuestionsApplicationSectionEnum.programs,
+          options: [
+            {
+              text: 'Not Permanent',
+              ordinal: 0,
+            },
+            {
+              text: 'Homeless',
+              ordinal: 1,
+            },
+            {
+              text: 'Do Not Consider',
+              ordinal: 2,
+            },
+            {
+              text: 'Prefer not to say',
+              ordinal: 3,
+            },
+          ],
+        },
+      }),
+    });
   await prismaClient.multiselectQuestions.create({
     data: multiselectQuestionFactory(lakeviewJurisdiction.id, {
       multiselectQuestion: {
@@ -1097,6 +1172,8 @@ export const stagingSeed = async (
     //       cityEmployeeQuestion,
     //       workInCityQuestion,
     //       multiselectQuestionPrograms,
+    //       mobilityAccessibilityNeedsProgramQuestion,
+    //       hearingVisionAccessibilityNeedsProgramQuestion,
     //     ],
     //     applications: [
     //       await applicationFactory({

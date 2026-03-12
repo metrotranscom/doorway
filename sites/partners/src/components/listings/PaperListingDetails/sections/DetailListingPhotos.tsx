bloom-housing/bloom

test: typed and dynamic listing cypress test (#5798)

sites/partners/src/components/listings/PaperListingDetails/sections/DetailListingPhotos.tsx
@@ -31,7 +31,11 @@ const DetailListingPhotos = () => {
       preview: {
         content: (
           <TableThumbnail>
-            <img src={getUrlForListingImage(image)} alt={t("listings.sections.photoTitle")} />
+            <img
+              src={getUrlForListingImage(image)}
+              alt={t("listings.sections.photoTitle")}
+              id={`listing-detail-image-${index}`}
+            />
           </TableThumbnail>
         ),
       },

metrotranscom/doorway

sites/partners/src/components/listings/PaperListingDetails/sections/DetailListingPhotos.tsx
@@ -31,7 +31,11 @@ const DetailListingPhotos = () => {
       preview: {
         content: (
           <TableThumbnail>
-            <img src={getImageUrlFromAsset(image)} alt={t("listings.sections.photoTitle")} />
+            <img
+              src={getImageUrlFromAsset(image)}
+              alt={t("listings.sections.photoTitle")}
+              id={`listing-detail-image-${index}`}
+            />
           </TableThumbnail>
         ),
       },
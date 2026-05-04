344f58dbf082675cc29789d75a6e9ed44abd061c
Morgan Ludtke <42942267+ludtkemorgan@users.noreply.github.com>
2026-05-04 16:35:46 -0500

    fix: edit account server side props (#1524)

--- sites/public/src/pages/account/edit.tsx
+++ sites/public/src/pages/account/edit.tsx
@@ -2,30 +2,20 @@ import React, { useContext } from "react"
 import { AuthContext } from "@bloom-housing/shared-helpers"
 import { EditPublicAccount } from "../../components/account/EditPublicAccount"
 import { EditAdvocateAccount } from "../../components/account/EditAdvocateAccount"
-import { fetchAgencies, fetchJurisdictionByName } from "../../lib/hooks"
-import { Agency } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
 
-interface EditProps {
-  agencies: Agency[]
-}
-
-const Edit = (props: EditProps) => {
+const Edit = () => {
   const { profile } = useContext(AuthContext)
-  return profile?.isAdvocate ? (
-    <EditAdvocateAccount agencies={props.agencies} />
-  ) : (
-    <EditPublicAccount />
-  )
+  return profile?.isAdvocate ? <EditAdvocateAccount agencies={[]} /> : <EditPublicAccount />
 }
 
 export default Edit
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function getServerSideProps(context: { req: any; query: any }) {
-  const jurisdiction = await fetchJurisdictionByName(context.req)
-  const agencies = await fetchAgencies(context.req, jurisdiction?.id)
+// export async function getServerSideProps(context: { req: any; query: any }) {
+//   const jurisdiction = await fetchJurisdictionByName(context.req)
+//   const agencies = await fetchAgencies(context.req, jurisdiction?.id)
 
-  return {
-    props: { jurisdiction, agencies: agencies?.items || [] },
-  }
-}
+//   return {
+//     props: { jurisdiction, agencies: agencies?.items || [] },
+//   }
+// }

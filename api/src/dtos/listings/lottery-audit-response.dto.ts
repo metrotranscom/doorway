8e49438f4a4ed3d5572362b7d04d6eefdb69b1e1
Eric Lott <ericblott@gmail.com>
2026-05-07 08:37:53 -0500

    refactor(api): address feedback for PR #1519 and simplify audit automation

--- api/src/dtos/listings/lottery-audit-response.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Expose } from 'class-transformer';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class LotteryAuditResponseDto {
-  @Expose()
-  @ApiProperty()
-  workQueueItemID: string;
-}
